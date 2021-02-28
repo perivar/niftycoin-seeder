0d85aa203935eabd3ab5df5f254469d8e28a5f6a
Per Ivar Nerseth <perivar@nerseth.com>
2021-02-28 21:19:27 +0100

    fixed

--- resources/fix-niftycoin-b15.sh
+++ resources/fix-niftycoin-b15.sh
@@ -40,6 +40,7 @@ GENESIS_REWARD_PUBKEY=044e0d4bc823e20e14d66396a64960c993585400c53f1e6decb273f249
 MAIN_PUB_KEY=040184710fa689ad5023690c80f3a49c8f13f8d45b8c857fbcbc8bc4a8e4d3eb4b10f4d4604fa08dce601aaf0f470216fe1b51850b4acf21b179c45070ac7b03a9
 MERKLE_HASH=ab4127c2db071267873ab33b6349d6c181a2aa79501848cba33c8c2460237a48
 TIMESTAMP=1613933282
+TIMESTAMP_TEST=1486949366
 BITS=0x1e0ffff0
 
 MAIN_NONCE=2085157839
@@ -85,6 +86,8 @@ LITECOIN_REGTEST_GENESIS_HASH=530827f38f93b43ed12af0b3ad25a288dc02ed74d6d7857862
 MINIMUM_CHAIN_WORK_MAIN=0x00000000000000000000000000000000000000000000002ebcfe2dd9eff82666
 MINIMUM_CHAIN_WORK_TEST=0x0000000000000000000000000000000000000000000000000007d006a402163e
 
+# Note! see also alot of hardcoded values for this branch in the replace statements
+
 COIN_NAME_LOWER=$(echo $COIN_NAME | tr '[:upper:]' '[:lower:]')
 COIN_NAME_UPPER=$(echo $COIN_NAME | tr '[:lower:]' '[:upper:]')
 COIN_UNIT_LOWER=$(echo $COIN_UNIT | tr '[:upper:]' '[:lower:]')
@@ -123,9 +126,57 @@ for i in $(find . -type f | grep -v "^./.git"); do
     $SED -i "s/Litoshis/$COIN_NAME_DENOM3/g" $i
 done
 
+# also fix .gitignore
+$SED -i "s/Litecoin/$COIN_NAME/g" .gitignore
+$SED -i "s/litecoin/$COIN_NAME_LOWER/g" .gitignore
+$SED -i "s/LITECOIN/$COIN_NAME_UPPER/g" .gitignore
+
 # now replace all ports
 $SED -i "s/9333/$MAINNET_PORT/g" src/chainparams.cpp contrib/qos/tc.sh contrib/seeds/generate-seeds.py doc/tor.md src/rpc/net.cpp test/functional/proxy_test.py
 $SED -i "s/19335/$TESTNET_PORT/g" src/chainparams.cpp contrib/qos/tc.sh contrib/seeds/generate-seeds.py doc/tor.md src/rpc/net.cpp test/functional/proxy_test.py
+
+# set supply and maturity
+$SED -i "s/84000000/$TOTAL_SUPPLY/" src/amount.h
+$SED -i "s/COINBASE_MATURITY = 100/COINBASE_MATURITY = $COINBASE_MATURITY/" src/consensus/consensus.h
+
+if [ -n "$PREMINED_AMOUNT" ]; then
+   $SED -i "s/CAmount nSubsidy = 50 \* COIN;/if \(nHeight == 1\) return COIN \* $PREMINED_AMOUNT;\n    CAmount nSubsidy = 50 \* COIN;/" src/validation.cpp
+fi
+
+# set check for reg testnet to genesis (this must go first since sero will math what we set them to)
+LITECOIN_REGTEST_CHECK_START=0
+REGTEST_CHECK_REPL="\
+\n            {\
+\n                {  0, uint256S(\"0x${REGTEST_GENESIS_HASH}\")},\
+\n            }\
+\n        "
+perl -0777 -p -i -w -e 's/(checkpointData = \(CCheckpointData\) \{)\s+\{\s+{\s*'"${LITECOIN_REGTEST_CHECK_START}"'.*?(?=\};)(\};)/$1'"${REGTEST_CHECK_REPL}"'$2/gs' src/chainparams.cpp
+
+# set check for mainnet to genesis
+LITECOIN_MAIN_CHECK_START=1500
+MAIN_CHECK_REPL="\
+\n            {\
+\n                {  0, uint256S(\"0x${MAIN_GENESIS_HASH}\")},\
+\n            }\
+\n        "
+perl -0777 -p -i -w -e 's/(checkpointData = \(CCheckpointData\) \{)\s+\{\s+{\s*'"${LITECOIN_MAIN_CHECK_START}"'.*?(?=\};)(\};)/$1'"${MAIN_CHECK_REPL}"'$2/gs' src/chainparams.cpp
+
+# set check for testnet to genesis
+LITECOIN_TEST_CHECK_START=2056
+TEST_CHECK_REPL="\
+\n            {\
+\n                {  0, uint256S(\"0x${TEST_GENESIS_HASH}\")},\
+\n            }\
+\n        "
+perl -0777 -p -i -w -e 's/(checkpointData = \(CCheckpointData\) \{)\s+\{\s+{\s*'"${LITECOIN_TEST_CHECK_START}"'.*?(?=\};)(\};)/$1'"${TEST_CHECK_REPL}"'$2/gs' src/chainparams.cpp
+
+# set tx check for mainnet to genesis
+# replace the numbers below 59, 15164006833 etc.
+perl -0777 -p -i -w -e 's/(chainTxData = ChainTxData\{)\s+\/\/ Data as of block 59.*?(?=1516406833,)\d+,(.*?(?=19831879,))\d+,(.*?(?=0.06))\d+.\d+(.*?(?=\};)\};)/${1}\n            '"${TIMESTAMP}"',${2}0,${3}0.0${4}/gs' src/chainparams.cpp
+
+# set tx check for testnet to genesis
+# replace the numbers below a0, 1516406749 etc.
+perl -0777 -p -i -w -e 's/(chainTxData = ChainTxData\{)\s+\/\/ Data as of block a0.*?(?=1516406749,)\d+,(.*?(?=794057,))\d+,(.*?(?=0.01))\d+.\d+(.*?(?=\};)\};)/${1}\n            '"${TIMESTAMP_TEST}"',${2}0,${3}0.0${4}/gs' src/chainparams.cpp
 
 # replace messagestart bytes for mainnet
 $SED -i "s/pchMessageStart\[0\] = 0xfb;/pchMessageStart[0] = $MAIN_MESSAGE_START_BYTE0;/" src/chainparams.cpp
@@ -145,7 +196,6 @@ $SED -i "s/pchMessageStart\[1\] = 0xbf;/pchMessageStart[1] = $REGTEST_MESSAGE_ST
 $SED -i "s/pchMessageStart\[2\] = 0xb5;/pchMessageStart[2] = $REGTEST_MESSAGE_START_BYTE2;/" src/chainparams.cpp
 $SED -i "s/pchMessageStart\[3\] = 0xda;/pchMessageStart[3] = $REGTEST_MESSAGE_START_BYTE3;/" src/chainparams.cpp
 
-$SED -i "s/84000000/$TOTAL_SUPPLY/" src/amount.h
 $SED -i "s/1,48/1,$PUBKEY_CHAR/" src/chainparams.cpp
 $SED -i "s/1,176/1,$PUBKEY_CHAR/" src/chainparams.cpp
 
@@ -175,26 +225,48 @@ $SED -i "s/$MINIMUM_CHAIN_WORK_MAIN/0x00/" src/chainparams.cpp
 $SED -i "s/$MINIMUM_CHAIN_WORK_TEST/0x00/" src/chainparams.cpp
 
 # change bip activation heights
+# each new improvement (bip) is activated at a future block to give people time to update their software
+# as we are creating a new chain, set these to genesis
+# mainnet
 # bip 16 (added in litecoin 0.16)
-# $SED -i "s/218579/0/" src/chainparams.cpp
+# $SED -i "s/BIP16Height = 218579/BIP16Height = 0/" src/chainparams.cpp
 # bip 34
-$SED -i "s/710000/0/" src/chainparams.cpp
-$SED -i "s/fa09d204a83a768ed5a7c8d441fa62f2043abf420cff1226c7b4329aeb9d51cf/$MAIN_GENESIS_HASH/" src/chainparams.cpp
+$SED -i "s/BIP34Height = 710000/BIP34Height = 0/" src/chainparams.cpp
+$SED -i "s!fa09d204a83a768ed5a7c8d441fa62f2043abf420cff1226c7b4329aeb9d51cf\");!$MAIN_GENESIS_HASH\"); // main genesis hash!" src/chainparams.cpp
 # bip 65
-$SED -i "s/918684/0/" src/chainparams.cpp
+$SED -i -r "s,BIP65Height = 918684;.*$,BIP65Height = 0;," src/chainparams.cpp
 # bip 66
-$SED -i "s/811879/0/" src/chainparams.cpp
+$SED -i -r "s,BIP66Height = 811879;.*$,BIP66Height = 0;," src/chainparams.cpp
+
+# testnet
+# bip 16 (added in litecoin 0.16)
+# $SED -i "s/BIP16Height = 76/BIP16Height = 0/" src/chainparams.cpp
+# bip 34
+$SED -i "s/BIP34Height = 76/BIP34Height = 0/" src/chainparams.cpp
+$SED -i "s!8075c771ed8b495ffd943980a95f702ab34fce3c8c54e379548bda33cc8c0573\");!$TEST_GENESIS_HASH\"); // test genesis hash!" src/chainparams.cpp
+# bip 65
+$SED -i -r "s,BIP65Height = 76;.*$,BIP65Height = 0;," src/chainparams.cpp
+# bip 66
+$SED -i -r "s,BIP66Height = 76;.*$,BIP66Height = 0;," src/chainparams.cpp
 
 # pow targets
-$SED -i "s,nPowTargetTimespan = 3.5 \* 24 \* 60 \* 60; // 3.5 days,nPowTargetTimespan = $POW_TARGET_TIMESPAN,g" src/chainparams.cpp
-$SED -i "s,nPowTargetSpacing = 2.5 \* 60;,nPowTargetSpacing = $POW_TARGET_SPACING,g" src/chainparams.cpp
+$SED -i -r "s,nPowTargetTimespan = 3.5 \* 24 \* 60 \* 60;.*$,nPowTargetTimespan = $POW_TARGET_TIMESPAN,g" src/chainparams.cpp
+$SED -i -r "s,nPowTargetSpacing = 2.5 \* 60;.*$,nPowTargetSpacing = $POW_TARGET_SPACING,g" src/chainparams.cpp
 
 # defaultAssumeValid
-$SED -i "s/0x59c9b9d3fec105bdc716d84caa7579503d5b05b73618d0bf2d5fa639f780a011/0x$MAIN_GENESIS_HASH/" src/chainparams.cpp
-$SED -i "s/0xa0afbded94d4be233e191525dc2d467af5c7eab3143c852c3cd549831022aad6/0x$TEST_GENESIS_HASH/" src/chainparams.cpp
+#$SED -i -r "s,0x59c9b9d3fec105bdc716d84caa7579503d5b05b73618d0bf2d5fa639f780a011.*$,0x$MAIN_GENESIS_HASH\"); // main genesis hash," src/chainparams.cpp
+#$SED -i -r "s,0xa0afbded94d4be233e191525dc2d467af5c7eab3143c852c3cd549831022aad6.*$,0x$TEST_GENESIS_HASH\"); // test genesis hash," src/chainparams.cpp
+#ref blockchain by example - set to zero
+$SED -i -r "s,0x59c9b9d3fec105bdc716d84caa7579503d5b05b73618d0bf2d5fa639f780a011.*$,0x00\");," src/chainparams.cpp
+$SED -i -r "s,0xa0afbded94d4be233e191525dc2d467af5c7eab3143c852c3cd549831022aad6.*$,0x00\");," src/chainparams.cpp
 
-if [ -n "$PREMINED_AMOUNT" ]; then
-   $SED -i "s/CAmount nSubsidy = 50 \* COIN;/if \(nHeight == 1\) return COIN \* $PREMINED_AMOUNT;\n    CAmount nSubsidy = 50 \* COIN;/" src/validation.cpp
-fi
+# set consesus
+# These values are supposed to state the time window during which deployment of a particular protocol change is allowed to happen. 
+# For regtest that window is set to be from times 0 to 999999999999, so that you can do it whenever you want.
+# Make sure these are enabled from day 1 (except regtest which we keep)
+perl -0777 -p -i -w -e 's/(consensus.vDeployments\[Consensus::DEPLOYMENT_\w+\].nStartTime).*/$1 = 0;/g' src/chainparams.cpp
+perl -0777 -p -i -w -e 's/(consensus.vDeployments\[Consensus::DEPLOYMENT_\w+\].nTimeout) = (?!999999999999ULL).*/$1 = '"${TIMESTAMP}"';/g' src/chainparams.cpp
 
-$SED -i "s/COINBASE_MATURITY = 100/COINBASE_MATURITY = $COINBASE_MATURITY/" src/consensus/consensus.h
+# remove all entries in chainparamsseeds.h
+perl -0777 -p -i -w -e 's/(static SeedSpec6 pnSeed6_\w+\[\] = \{).*?(?=\};)\};/$1};/gs' src/chainparamsseeds.h
+
